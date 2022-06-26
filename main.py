8eb3fe70f3603da631778bd091d48b266f7b7dfd
sts <sts@informatik.uni-kiel.de>
2022-06-26 09:06:13 +0200

    created gitignore, added data folder

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #
 # Given:
 # (1) An (incomplete) db export of image names (id), years of creation and authors.
-# (2) A folder full of images named by id. Turns out some id_ name subimages exist, which aren't listed in (1) yet.
+# (2) A folder (Data/imgs) full of images named by id. Some id_subimages exist, which aren't listed in (1) yet.
 #
 # Goal:
 # (1) Sort images in folder by year with the following steps
