12a6af3db15dbc4f2c6c16645c0371fb8069fff1
sts <sts@informatik.uni-kiel.de>
2022-06-26 08:31:22 +0200

    Created main file, added short description of given, goal and approach.

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a script for the MedHist Project.
+#
+# Given:
+# (1) An (incomplete) db export of image names (id), years of creation and authors.
+# (2) A folder full of images named by id. Turns out some id_ name subimages exist, which aren't listed in (1) yet.
+#
+# Goal:
+# (1) Sort images in folder by year with the following steps
+#
+# Approach:
+# (1) complete given list of images (1)
+# (2) rename images in folder to year_author_id
+# (3) reupload new folder for requesting person
+
+
