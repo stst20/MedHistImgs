a3506e15693b3ad326bc30bc8029e75e328836d4
sts <sts@informatik.uni-kiel.de>
2022-06-26 09:55:21 +0200

    created csv merge output

--- main.py
+++ main.py
@@ -13,3 +13,33 @@
 # (3) reupload new folder for requesting person
 
 
+from os import listdir
+import pandas as pd
+
+
+# Load image names in list
+img_path = "Data/imgs"
+file_list = [f for f in listdir(img_path)]
+no_ext_list = [x.split('.')[0] for x in file_list]
+# print(file_list)
+print(no_ext_list)
+print(len(no_ext_list))
+
+inv_nr = no_ext_list
+datierung = ["-"] * len(no_ext_list)
+kuenstler = ["-"] * len(no_ext_list)
+aux_dict = {"Inv.Nr.": inv_nr, "Datierung": datierung, "Künstler": kuenstler}
+
+img_folder_df = pd.DataFrame(aux_dict)
+# img_folder_df = pd.DataFrame(no_ext_list, columns=["Inv.Nr.", "Datierung", "Künstler"])
+print(img_folder_df)
+
+
+# Load excel list using pandas data frame
+dbinfo_df = pd.read_excel (r'Data\Medhist_Invnr_Dat_Kuenstler.xlsx')
+print (dbinfo_df)
+
+merged_df = pd.merge(dbinfo_df, img_folder_df, on="Inv.Nr.", how="outer")
+print(merged_df)
+
+merged_df.to_csv("Data/merged_output.csv", sep=";", encoding='utf-8')
